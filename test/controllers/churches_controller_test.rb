e2803da41502975a87fc003a5d6c2fb5561a07f8
Krasho <crasho1481@gmail.com>
2018-05-31 16:17:38 -0500

    I created the church controller

--- test/controllers/churches_controller_test.rb
+++ test/controllers/churches_controller_test.rb
@@ -1,9 +1,38 @@
 require 'test_helper'
 
 class ChurchesControllerTest < ActionDispatch::IntegrationTest
-  test "should get Church" do
-    get churches_Church_url
+  setup do
+    @church = churches(:one)
+  end
+
+  test "should get index" do
+    get churches_url, as: :json
     assert_response :success
   end
 
+  test "should create church" do
+    assert_difference('Church.count') do
+      post churches_url, params: { church: {  } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show church" do
+    get church_url(@church), as: :json
+    assert_response :success
+  end
+
+  test "should update church" do
+    patch church_url(@church), params: { church: {  } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy church" do
+    assert_difference('Church.count', -1) do
+      delete church_url(@church), as: :json
+    end
+
+    assert_response 204
+  end
 end
